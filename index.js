815e8bf674654afdb38f52f1271ee41162235ad6
Bui Cao Vinh <buicaovinh5@gmail.com>
2025-12-19 11:00:03 +0700

    ver3

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get("/", async (req, res) => {
     res.status(200).send("Xin chào bạn");
 });
 
+
 // Start server
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
